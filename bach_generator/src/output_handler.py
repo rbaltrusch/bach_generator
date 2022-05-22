e40954c86b0c2f458b6b75bb0fae1ff4f8c6be24
Richard Baltrusch <richard@baltrusch.net>
2022-05-22 12:16:38 +0200

    Add typehints

--- bach_generator/src/output_handler.py
+++ bach_generator/src/output_handler.py
@@ -17,19 +17,19 @@ class OutputHandler:
     def __init__(self):
         self.directory: str = ""
 
-    def setup_output_directory(self, directory: str):
+    def setup_output_directory(self, directory: str) -> None:
         """Sets up a timestamped subdirectory in the passed directory"""
         date = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
         self.directory = os.path.join(directory, date)
         if not os.path.isdir(self.directory):
             os.makedirs(self.directory)
 
-    def copy_files(self, *filepaths):
+    def copy_files(self, *filepaths: str) -> None:
         """Copies all filepaths to the output handler directory"""
         for filepath in filepaths:
             destination_path = os.path.join(self.directory, os.path.basename(filepath))
             shutil.copyfile(filepath, destination_path)
 
-    def write(self, score: music21.stream.Score, filename: str):
+    def write(self, score: music21.stream.Score, filename: str) -> None:
         """Writes the specified score to the output directory under the specified filename"""
         score.write("midi", os.path.join(self.directory, filename))
