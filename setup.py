8b8fe0af4a141beda6fae90ddd6d1f3eb1db5a19
Richard Baltrusch <richard@baltrusch.net>
2024-06-16 12:29:14 +0200

    Update workflows to support Python 3.11 and 3.12 (#52)
    This updates workflow files to support Python 3.11 and 3.12.
    
    Also bumps the setup.py version to 1.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ project_dir = Path(__file__).parent
 
 setuptools.setup(
     name="bach_generator",
-    version="1.0.2",
+    version="1.1.0",
     description="Machine learning based Bach music generator",
     long_description=project_dir.joinpath("README.md").read_text(encoding="utf-8"),
     long_description_content_type="text/markdown",
@@ -39,6 +39,9 @@ setuptools.setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        # 3.12 doesn't work with setup.py, see: https://stackoverflow.com/questions/73533994/sub-package-not-importable-after-installation  pylint: disable=line-too-long
+        # "Programming Language :: Python :: 3.12",
         "Topic :: Artistic Software",
         "Topic :: Desktop Environment",
         "Topic :: Multimedia",
